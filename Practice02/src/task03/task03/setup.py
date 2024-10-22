8ec324fa59240e58700f79d33f3e7be33dd51e8f
Igor.Manninen <igor.manninen@jetbrains.com>
2024-10-22 22:51:46 +0300

    solution 3

---
+++ Practice02/src/task03/task03/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'task03'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + "/launch", ['launch/task03.launch']),
+        ('share/' + package_name + "/config", ['config/task03.yaml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'service = task03.my_service_node:main'
+        ],
+    },
+)
