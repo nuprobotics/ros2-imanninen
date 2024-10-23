c1b37a106e0b1df93066517327650ef1e3d908cf
Igor.Manninen <igor.manninen@jetbrains.com>
2024-10-23 17:43:52 +0300

    solution 3.7

--- Practice02/build/task03/build/lib/task03/my_service_node.py
+++ Practice02/build/task03/build/lib/task03/my_service_node.py
@@ -5,43 +5,34 @@ from std_srvs.srv import Trigger
 class MyService(Node):
     def __init__(self):
         super(MyService, self).__init__("my_service")
-        self.declare_parameter('service_name', '/trigger_service')
-        self.declare_parameter('default_string', 'No service available')
-
-        self.service_name = self.get_parameter('service_name').get_parameter_value().string_value
-        self.default_string = self.get_parameter('default_string').get_parameter_value().string_value
-
-        self.stored_string = "default_value"
+        self.declare_parameter("service_name", "")
+        self.declare_parameter("default_string", "")
+        self.service_name = self.get_parameter("service_name").get_parameter_value().string_value
+        self.default_string = self.get_parameter("default_string").get_parameter_value().string_value
         self.client = self.create_client(Trigger, "/spgc/trigger")
+        self.string_to_store = ""
+        self.service = self.create_service(Trigger, self.service_name, self.service_cb)
 
-        if not self.client.wait_for_service(1.0):
-            self.get_logger().info(f"Service {self.service_name} not available. Using default value.")
-            self.stored_string = self.default_string
-            self.get_logger().info(f"Updated stored string. {self.stored_string}")
+        if not self.client.wait_for_service(timeout_sec=1.0):
+            # self.get_logger().info(f'service {self.service_name} not available -> {self.default_string}')
+            self.string_to_store = self.default_string
         else:
             future = self.client.call_async(Trigger.Request())
-            self.get_logger().info(" Waiting for service ... ")
+            # self.get_logger().info("Waiting for service ... ")
             rclpy.spin_until_future_complete(self, future)
-            self.stored_string = future.result().message
-            self.get_logger().info(f"Updated stored string: {self.stored_string}")
-
-        self.service = self.create_service(Trigger, self.service_name, self.service_cb)
+            self.string_to_store = future.result().message
 
     def service_cb(self, request, response):
         response.success = True
-        response.message = self.stored_string
+        response.message = self.string_to_store
         return response
 
 def main(args=None):
     rclpy.init(args=args)
     my_service = MyService()
-    try:
-        rclpy.spin(my_service)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        my_service.destroy_node()
-        rclpy.shutdown()
+    rclpy.spin(my_service)
+    my_service.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
